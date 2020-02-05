c9a0ee70debefa2a541bf181b98042c178ed9fa6
Nik <56631042+iamitaliya@users.noreply.github.com>
2020-02-05 20:17:49 +0100

    adding the code

---
+++ cmake-build-debug/CMakeFiles/3D_RRT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3D_RRT.exe"
+  "3D_RRT.exe.manifest"
+  "3D_RRT.pdb"
+  "CMakeFiles/3D_RRT.dir/main.cpp.obj"
+  "lib3D_RRT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D_RRT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
